1eb7b1bd5ecf56da0d38444c083cf37ff7f9515b
ssowers2 <sabriyasowers@gmail.com>
2023-12-31 00:10:51 -0600

    Created part of the CustomUserDetailsService class

---
+++ src/main/java/com/launchcode/munchincrunch/security/CustomUserDetailsService.java
@@ -0,0 +1,17 @@
+package com.launchcode.munchincrunch.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // Implement logic to load user details from the database
+        return null; // Replace with actual UserDetails
+    }
+
+}
--- src/main/java/com/launchcode/munchincrunch/security/UserAuthentication.java
+++ src/main/java/com/launchcode/munchincrunch/security/UserAuthentication.java
@@ -2,34 +2,49 @@ package com.launchcode.munchincrunch.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@Configuration //this indicates class type
-@EnableWebSecurity //this enables/configures web security in app
+
+@Configuration // indicates class type
+@EnableWebSecurity // enables/configures web security in app
 public class UserAuthentication extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    /**
+     Bean method using BCryptPasswordEncoder from Spring Security to create a secure code for passwords.
+     This code, difficult to crack, is stored in the database for secure storage.
+     During login or password checks, the stored secret code is used to verify and ensure password safety.
+     */
     @Bean
-    public PasswordEncoder passwordEncoder() { //method encode passwords for
-        // secure storage in MySQL
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //overridden method used to configure security settings for different
-        // HTTP requests
+        //overridden method used to config security settings for HTTP requests
         http
                 .authorizeRequests()
                 .antMatchers("/", "/public/**").permitAll()
-                //indicates requests to the root path ("/") and any path
-                // under "/public" is allowed
-                // without authentication.
-                .anyRequest().authenticated() //indicates any other request
-                // should be authenticated.
+                //Allows access to the root path ("/") and any path
+                // under "/public" without authentication.
+                .antMatchers("/register").permitAll()
+                //User registration allowed without authentication
+                .anyRequest().authenticated()
+                //indicates any other request requires authentication
                 .and()
                 .formLogin()//Configures form-based login
                 .loginPage("/login")//Specifies the custom login page URL ("/login").
@@ -39,9 +54,10 @@ public class UserAuthentication extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/")//Specifies the URL to redirect to after logout
                 .permitAll(); //Allows users to perform logout.
     }
+
 }
 
 //This code should allow users access to the root ("/") and any path under
-// ("/public") but all other user access require authentication. Form based
+// ("/public") but all other access requires authentication. Form based
 // login is enabled with a custom login page, logout is config to redirect to
 // root.
